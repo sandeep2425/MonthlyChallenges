d15063ca500974791a58da61d37dea390ecbf2be
Sandeep <sai.sandeep2425@gmail.com>
2025-08-17 13:40:07 +0530

    frontend url is updated

--- MonthChallenges/src/main/java/com/spring/MonthChallenges/Services/CorsConfig.java
+++ MonthChallenges/src/main/java/com/spring/MonthChallenges/Services/CorsConfig.java
@@ -1,18 +1,23 @@
 package com.spring.MonthChallenges.Services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173") // Vite default port
+                    .allowedOrigins(frontendUrl) // Vite default port
                     .allowedMethods("*")
                     .allowedHeaders("*");
             }
